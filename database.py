9f62fd8c39f8057a1ce41f0bc8cc084609edff17
Jonathan Martin <jonathanmartin@192.168.1.35>
2025-12-25 20:53:31 -0300

    Primera versión: Backend modular con FastAPI y SQLModel

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, create_engine, Session
+
+# 1. Nombre del archivo físico
+sqlite_file_name = "billetera_v2.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+# 2. El Motor (Engine)
+engine = create_engine(sqlite_url)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
---
+++ models.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, Field
+
+# Solo definimos la clase. 
+class Movimiento(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    monto: float
+    tipo: str
+    concepto: str
