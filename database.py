2ea1404edcb5328c836725ecc92fc783b03310db
Jonathan Martin <jonathanmartin@192.168.1.17>
2025-12-30 17:44:15 -0300

    Feat: Migración a PostgreSQL con Docker Compose y lógica de reintentos

--- database.py
+++ database.py
@@ -1,16 +1,32 @@
 from sqlmodel import SQLModel, create_engine, Session
+from settings import settings
 
-# 1. Nombre del archivo físico
-sqlite_file_name = "billetera_v2.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+connection_string = settings.database_url
 
-# 2. El Motor (Engine)
-engine = create_engine(sqlite_url)
+connect_args = {}
+
+if connection_string.startswith("sqlite"):
+    connect_args["check_same_thread"] = False
+
+engine = create_engine(connection_string, echo=True, connect_args=connect_args)
 
 def create_all_tables():
-    SQLModel.metadata.create_all(engine)
+    max_retries = 10      # Intentar 10 veces
+    wait_seconds = 2      # Esperar 2 segundos entre intentos
 
-
+    for i in range(max_retries):
+        try:
+            print(f"🔄 Intentando conectar a la DB (Intento {i+1}/{max_retries})...")
+            SQLModel.metadata.create_all(engine)
+            print("✅ Conexión exitosa. Tablas creadas.")
+            return  
+        except OperationalError:
+            print(f"⏳ La base de datos no está lista. Esperando {wait_seconds}s...")
+            time.sleep(wait_seconds)
+    
+    # Si llega aquí, falló todas las veces
+    print("❌ Error crítico: No se pudo conectar a la base de datos después de varios intentos.")
+    raise Exception("Database Connection Failed")
 def get_session():
     with Session(engine) as session:
         yield session
--- settings.py
+++ settings.py
@@ -4,15 +4,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     # Definimos las variables que ESPERAMOS encontrar
     limite_transferencia: int
-    nombre_app: str = "Billetera Local"  # Valor por defecto si no está en el .env
-
+    nombre_app: str = "Billetera Local"
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
 
-    # Configuración para que lea el archivo .env automáticamente
+# Si no existe en el .env, usará SQLite por defecto
+    database_url: str = "sqlite:///./billetera_v2.db"
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
-# Instanciamos la clase una sola vez para importarla desde otros lados
 settings = Settings()
